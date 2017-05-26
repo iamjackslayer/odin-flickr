b1531720b3b4d86d41713717b9e51a92bee8da04
Jack Wong <jackwong3101@yahoo.com>
2017-05-26 11:34:12 +0800

    replace flickr-fu with flicraw

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,19 @@
-require 'flickr_fu'
+require 'flickraw'
 class StaticPagesController < ApplicationController
-	def home 
 
+	def home 
+		FlickRaw.api_key="02ec0f82e01c354e9b237f87b31ffd34"
+		FlickRaw.shared_secret="8db725aa215775af"
+
+		@list = flickr.photos.getRecent
+		@id = list[0].id
+		@secret = list[0].secret
+		@info = flickr.photos.getInfo :photo_id => @id, :secret => @secret
+
+		
 	end
 	def create
-		@flickr = Flickr.new(Rails.root.join('config','flickr.yml'))
-		@photos = @flickr.photos.search(:tags => 'ruby-flickr')
+		
 		render 'home'
 	end
 end
