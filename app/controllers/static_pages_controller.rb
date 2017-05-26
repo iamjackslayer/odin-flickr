00dd55ee9368df3b55754d9034270d0f282aa13a
Jack Wong <jackwong3101@yahoo.com>
2017-05-26 12:37:48 +0800

    include photos array to show photo url

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,10 @@ class StaticPagesController < ApplicationController
 		FlickRaw.api_key="02ec0f82e01c354e9b237f87b31ffd34"
 		FlickRaw.shared_secret="8db725aa215775af"
 
-		@list = flickr.photos.getRecent
-		@id = @list[0].id
-		@secret = @list[0].secret
-		@info = flickr.photos.getInfo :photo_id => @id, :secret => @secret
+		# @list = flickr.photos.getRecent
+		# @id = @list[0].id
+		# @secret = @list[0].secret
+		@photos = flickr.photos.search(user_id: "9732441@N05")
 
 
 	end
