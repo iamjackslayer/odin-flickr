6d7095dde93ee4131381c1d80b2ad058d112af82
Jack Wong <jackwong3101@yahoo.com>
2017-05-26 14:04:24 +0800

    .

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,10 @@ class StaticPagesController < ApplicationController
 		FlickRaw.api_key="02ec0f82e01c354e9b237f87b31ffd34"
 		FlickRaw.shared_secret="8db725aa215775af"
 
-		@user_id = params[:user][:id]
-		@photos = flickr.photos.search(user_id: @user_id, extra: "url") if @user_id
+		if params
+			@user_id = params[:user][:id]
+		end
+		@photos = flickr.photos.search(user_id: @user_id) if @user_id
 
 
 	end
