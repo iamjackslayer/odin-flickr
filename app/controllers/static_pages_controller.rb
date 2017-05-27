243b4f58681b68737cbee6be440ee5ab1f9b52e6
Jack Wong <jackwong3101@yahoo.com>
2017-05-27 13:21:11 +0800

    .

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,10 +8,10 @@ class StaticPagesController < ApplicationController
 		api_key = "02ec0f82e01c354e9b237f87b31ffd34"
 		secret = "8db725aa215775af"
 		if params.has_key?(:user)
-			if !params[:user][:id] == ""
+			if !(params[:user][:id] == "")
 				@user_id = params[:user][:id]
 				@photos = flickr.photos.search(user_id: @user_id) 
-			elsif !params[:user][:user_name] == ""
+			elsif !(params[:user][:user_name] == "")
 				@user_name = params[:user][:username]
 				@user = flickr.people.findByUsername(username: @user_name)
 				@photos = flickr.photos.search(user_id: @user.id)
